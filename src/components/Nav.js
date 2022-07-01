c041bba6dd5ca934be81a78806442ba4cd6d8d10
chrisgth <ksemkus00@gmail.com>
2022-07-01 19:40:33 +0300

    Introduction styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import menuIcon from "../images/menu.svg";
 
 function Nav() {
   return (
     <div className="nav">
       <div className="navlist">
-        <a href="">Skills</a>
+        <a href=".skills">Skills</a>
         <a href="">Projects</a>
         <a href="">Contact</a>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,15 @@
 function Skills() {
-  return <div className="skills"></div>;
+  return (
+    <div className="skills">
+      <p>My skill list includes...</p>
+      <div className="mernicons">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
